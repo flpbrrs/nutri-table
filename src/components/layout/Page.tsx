0710377d5440742203794e29bfd68d9543c290de
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-16 19:25:52 -0300

    style: alterando o estilo do componente Page

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -6,7 +6,7 @@ interface PageProps {
 
 export default function Page(props: PageProps) {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen pb-8">
             <div className="bg-blp-800 h-48 rounded-bl-3xl rounded-br-3xl" />
             <div className="container mx-auto px-32 h-full -mt-36">
                 {props.children}
