49e5f212425cee5577c671406baaf3afbaa0e64e
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-17 21:49:36 -0300

    refactor: ajustando a página principal para fazer uso das novas funcionalidades dos componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Card from "@/components/layout/Card";
 import Header from "@/components/layout/Header";
 import Page from "@/components/layout/Page";
 import { Table, TableBody, TableHeader, TableItem, TableRow } from "@/components/layout/Table";
-import { IconBowlChopsticks, IconBurger, IconMeat, IconToolsKitchen } from "@tabler/icons-react";
+import { IconBowlChopsticks, IconBurger, IconFridge, IconMeat, IconPlus, IconToolsKitchen } from "@tabler/icons-react";
 
 export default function Home() {
     return (
@@ -11,6 +11,11 @@ export default function Home() {
                 title="Dashboard de Nutrição Diária"
                 subtitle="Controle a sua ingestão de calorias de forma fácil"
                 larger
+                renderActions={() => (
+                    <button type="button" className="text-sm hover:underline hover:text-emerald-200">
+                        Editar Meta
+                    </button>
+                )}
             />
             <div className="flex gap-4 justify-between my-8">
                 <Card label="Calorias Totais" icon={<IconToolsKitchen />} unit="Calorias">
@@ -26,12 +31,31 @@ export default function Home() {
                     10
                 </Card>
             </div>
-            <Header title="Registre os alimentos do dia" />
+            <Header
+                title="Registre os alimentos do dia"
+                renderActions={() => (
+                    <div className="flex space-x-3">
+                        <button
+                            type="button"
+                            className="hover:scale-110 transition-transform text-gray-400 hover:text-gray-50"
+                        >
+                            <IconFridge stroke={2} />
+                        </button>
+                        <button
+                            type="button"
+                            className="flex gap-1 items-center p-1 rounded-lg bg-emerald-500 hover:bg-emerald-600 hover:scale-110 transition-transform"
+                        >
+                            <IconPlus className="size-full" />
+                        </button>
+                    </div>
+                )}
+            />
             <Table>
                 <TableHeader>
                     <TableRow>
                         <TableItem scope="col" data="Alimento" />
                         <TableItem scope="col" data="Quantidade" />
+                        <TableItem scope="col" data="Calorias" />
                         <TableItem scope="col" data="Proteinas" />
                         <TableItem scope="col" data="Carboidratos" />
                         <TableItem scope="col" data="Gorduras" />
@@ -39,11 +63,12 @@ export default function Home() {
                 </TableHeader>
                 <TableBody>
                     <TableRow>
-                        <TableItem scope="row" data="Arroz" />
-                        <TableItem scope="row" data="80g" />
-                        <TableItem scope="row" data="4g" />
-                        <TableItem scope="row" data="76g" />
-                        <TableItem scope="row" data="0g" />
+                        <TableItem scope="row" data="Arroz branco cozido" />
+                        <TableItem scope="row" data="100g" />
+                        <TableItem scope="row" data="124kcal" />
+                        <TableItem scope="row" data="2.6g" />
+                        <TableItem scope="row" data="1g" />
+                        <TableItem scope="row" data="25.8g" />
                     </TableRow>
                 </TableBody>
             </Table>
