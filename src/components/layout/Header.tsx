80c01d95a5c1300d6c08eef6aaa1d874fefd31a0
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-17 21:47:32 -0300

    refactor: Ajustando a importação dos tipos e o uso de componentes de renderização no componente Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,24 @@
+import type { ReactNode } from "react"
+
 interface HeaderProps {
     title: string
     subtitle?: string
     larger?: boolean
+    renderActions?: () => ReactNode
 }
 
-export default function Header({ title, subtitle, larger = false }: HeaderProps) {
+export default function Header({ title, subtitle, larger = false, renderActions = () => null }: HeaderProps) {
     return (
-        <div>
-            <p className={`font-extrabold ${larger ? 'text-3xl' : 'text-xl'}`}>
-                {title}
-            </p>
-            <small className={`text-slate-400 font-bold ${larger ? 'text-base' : 'text-xs'}`}>
-                {subtitle}
-            </small>
+        <div className="flex justify-between items-center">
+            <div>
+                <p className={`font-extrabold ${larger ? 'text-3xl' : 'text-xl'}`}>
+                    {title}
+                </p>
+                <small className={`text-slate-400 font-bold ${larger ? 'text-base' : 'text-xs'}`}>
+                    {subtitle}
+                </small>
+            </div>
+            {renderActions()}
         </div>
     )
 }
